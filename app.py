8f0b1cff2a97fcd473d5469f2d8a8484486eb195
sooominnn <lsm980830@gmail.com>
2022-11-10 11:19:03 +0900

    대학원 합격 예측하기

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+
 import pandas as pd
 
 data = pd.read_csv('gpascore.csv')
-print(data)
 
+# print(data.isnull().sum())
+data = data.dropna()
+
+y데이터 = data['admit'].values
+
+x데이터 = []
+
+# data라는 데이터프레임을 가로 한 줄씩 출력
+for i, rows in data.iterrows():
+  x데이터.append([rows['gre'], rows['gpa'], rows['rank']])
+
+
+import numpy as np
 import tensorflow as tf
-
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(64, activation='tanh'),
     tf.keras.layers.Desnse(128, activation='tanh'),
@@ -13,4 +25,4 @@ model = tf.keras.models.Sequential([
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-model.fit()
+model.fit(np.array(x데이터), np.array(y데이터), epochs=10)
